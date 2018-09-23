1f7fcd2dd979f9eaa75a3c6032ba3e90a3bd559e
toannh5@fsoft.com.vn <Password@01>
2018-09-23 11:19:28 +0700

    Git commit

--- Startup.cs
+++ Startup.cs
@@ -111,7 +111,7 @@ namespace NetCore
                     template: "Product/{id?}",
                     defaults: new { controller = "Product", action = "GetById" });
             });
-//Toan NH
+
         }
     }
 }
